7a0e509859db68fd9a9cc40cbc2422cecd495193
MathIsFun0 <89877664+MathIsFun0@users.noreply.github.com>
2022-04-30 21:51:13 -0400

    Delete scramble.py

--- scramble.py
+++
@@ -1,8 +0,0 @@
-async def scramble(ctx, arg):
-  from random import shuffle
-  if arg == "":
-    await ctx.send("Why are you guys all trying to find bugs? :(")
-    return
-  l = list(arg)
-  shuffle(l)
-  await ctx.send("".join(l))
