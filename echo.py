bba2a1e30c35b0fdd65e59fc8102e6146fd8fe68
MathIsFun0 <89877664+MathIsFun0@users.noreply.github.com>
2022-04-30 21:50:24 -0400

    Delete echo.py

--- echo.py
+++
@@ -1,7 +0,0 @@
-async def echo(ctx, arg = ""):
-    if arg.startswith("\\\\echo"):
-        pass # If an achievement system is added, this could be an achievement for bypassing recursion and "breaking" the bot.
-    if not arg.startswith("\\echo "):
-        await ctx.send(arg)
-    else:
-        await ctx.send("Will not echo a recursive command")
